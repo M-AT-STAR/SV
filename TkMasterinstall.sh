c15337e0a510670f461964ed1389c038bd079b6f
M-AT-STAR <772mastersunlimited@gmail.com>
2026-03-18 08:25:44 +0200

    Update TkMasterinstall.sh

--- TkMasterinstall.sh
+++ TkMasterinstall.sh
@@ -17,7 +17,7 @@ _tmp=$(mktemp)
 curl -sL "https://raw.githubusercontent.com/M-AT-STAR/SV/main/lckd_nstll" | openssl aes-256-cbc -d -a -pbkdf2 -pass pass:2026 > "$_tmp" 2>/dev/null
 
 if grep -q "Initializing" "$_tmp"; then
-    echo -e "\e[1;32m[+] Decryption successful. Booting setup...\e[0m"
+    echo -e "\e[1;32m[+] INSTALLATION SUCCESSFUL. Booting setup...\e[0m"
     sleep 1
     bash "$_tmp"
     rm -f "$_tmp"
